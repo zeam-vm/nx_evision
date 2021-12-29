d98ab1b599969ebbef64fd04ada8c892a918a6ef
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-29 15:35:11 +0900

    set version to 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NxEvision.MixProject do
   def project do
     [
       app: :nx_evision,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.12-dev",
       start_permanent: Mix.env() == :prod,
       deps: deps()
