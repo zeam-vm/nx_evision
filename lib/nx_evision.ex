51ad0852b5679bd910292d0a018e0bd7b25209d7
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-29 19:35:00 +0900

    implement convert functions

--- lib/nx_evision.ex
+++ lib/nx_evision.ex
@@ -4,4 +4,113 @@ defmodule NxEvision do
   between [Nx](https://github.com/elixir-nx/nx)
   and [evision](https://github.com/cocoa-xu/evision).
   """
+
+  @spec convert_nx_to_mat(Nx.t(), atom) :: {:ok, reference()} | {:error, String.t()}
+  def convert_nx_to_mat(t), do: convert_nx_to_mat(t, :RGB)
+
+  def convert_nx_to_mat(nil, _), do: {:error, "tensor is nil"}
+
+  def convert_nx_to_mat(t, colorspace) do
+    {rows, cols, channels} = Nx.shape(t)
+
+    case convert_nx_to_mat(Nx.to_binary(t), Nx.type(t), cols, rows, channels, colorspace) do
+      {:ok, reference} -> {:ok, reference}
+      {:error, reason} -> {:error, List.to_string(reason)}
+    end
+  end
+
+  @spec convert_nx_to_mat(
+          binary(),
+          {atom(), pos_integer()},
+          pos_integer(),
+          pos_integer(),
+          pos_integer(),
+          atom()
+        ) :: {:ok, reference()} | {:error, charlist()}
+  def convert_nx_to_mat(binary, type, cols, rows, channels = 3, :RGB) do
+    case OpenCV.Mat.from_binary(binary, type, cols, rows, channels) do
+      {:ok, reference} ->
+        case OpenCV.cvtcolor(reference, OpenCV.cv_color_rgb2bgr()) do
+          {:ok, reference} -> {:ok, reference}
+          {:error, reason} -> {:error, reason}
+          _ -> {:error, 'unknown error when cvtcolor'}
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+
+      _ ->
+        {:error, 'unknown error when from_binary'}
+    end
+  end
+
+  def convert_nx_to_mat(binary, type, cols, rows, channels = 4, :RGBA) do
+    case OpenCV.Mat.from_binary(binary, type, cols, rows, channels) do
+      {:ok, reference} ->
+        case OpenCV.cvtcolor(reference, OpenCV.cv_color_rgba2bgra()) do
+          {:ok, reference} -> {:ok, reference}
+          {:error, reason} -> {:error, reason}
+          _ -> {:error, 'unknown error when cvtcolor'}
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+
+      _ ->
+        {:error, 'unknown error when from_binary'}
+    end
+  end
+
+  def convert_nx_to_mat(binary, type, cols, rows, channels = 3, :BGR) do
+    case OpenCV.Mat.from_binary(binary, type, cols, rows, channels) do
+      {:ok, reference} -> {:ok, reference}
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, 'unknown error when from_binary'}
+    end
+  end
+
+  def convert_nx_to_mat(binary, type, cols, rows, channels = 4, :BGRA) do
+    case OpenCV.Mat.from_binary(binary, type, cols, rows, channels) do
+      {:ok, reference} -> {:ok, reference}
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, 'unknown error when from_binary'}
+    end
+  end
+
+  @spec convert_mat_to_nx(reference, atom) :: {:ok, Nx.t()} | {:error, String.t()}
+
+  def convert_mat_to_nx(mat), do: convert_mat_to_nx(mat, :RGB)
+
+  def convert_mat_to_nx(nil, _), do: {:error, "reference is nil"}
+
+  def convert_mat_to_nx(mat, :BGR) do
+    case {OpenCV.Mat.type(mat), OpenCV.Mat.shape(mat), OpenCV.Mat.to_binary(mat)} do
+      {{:ok, type}, {:ok, shape}, {:ok, binary}} ->
+        {
+          :ok,
+          Nx.from_binary(binary, type) |> Nx.reshape(shape)
+        }
+
+      _ ->
+        {:error, "Unknown Mat type"}
+    end
+  end
+
+  def convert_mat_to_nx(mat, :BGRA), do: convert_mat_to_nx(mat, :BGR)
+
+  def convert_mat_to_nx(mat, :RGB) do
+    case OpenCV.cvtcolor(mat, OpenCV.cv_color_bgr2rgb()) do
+      {:ok, reference} -> convert_mat_to_nx(reference, :BGR)
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, "Unknown error when cvtcolor"}
+    end
+  end
+
+  def convert_mat_to_nx(mat, :RGBA) do
+    case OpenCV.cvtcolor(mat, OpenCV.cv_color_bgra2rgba()) do
+      {:ok, reference} -> convert_mat_to_nx(reference, :BGRA)
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, "Unknown error when cvtcolor"}
+    end
+  end
 end
--- test/nx_evision_test.exs
+++ test/nx_evision_test.exs
@@ -1,4 +1,177 @@
 defmodule NxEvisionTest do
   use ExUnit.Case
   doctest NxEvision
+
+  describe "convert_nx_to_mat and imwrite bmp" do
+    test "size = {1, 1}, light purple RGB" do
+      img = Nx.tensor([[[0x9F, 0x5A, 0xAE]]], type: {:u, 8})
+      file = "/tmp/test_1_1.bmp"
+
+      case NxEvision.convert_nx_to_mat(img) do
+        {:error, reason} ->
+          assert {:error, reason} == false
+
+        {:ok, mat} ->
+          case OpenCV.imwrite(file, mat) do
+            {:error, reason} ->
+              assert {:error, reason} == false
+
+            :ok ->
+              assert File.exists?(file) == true
+
+              {file_str, file_exit} = System.cmd("file", [file])
+              assert file_exit == 0
+              assert String.match?(file_str, ~r/PC bitmap/) == true
+
+              {ok_error, binary} = File.read(file)
+
+              assert ok_error == :ok
+
+              assert binary ==
+                       <<66, 77, 58, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0, 40, 0, 0, 0, 1, 0, 0, 0, 1,
+                         0, 0, 0, 1, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 174, 90, 159, 0>>
+
+              case File.rm(file) do
+                :ok -> :ok
+                _ -> :ok
+              end
+          end
+      end
+    end
+
+    test "size = {1, 1}, light purple BGR" do
+      img = Nx.tensor([[[0xAE, 0x5A, 0x9F]]], type: {:u, 8})
+      file = "/tmp/test_1_1.bmp"
+
+      case NxEvision.convert_nx_to_mat(img, :BGR) do
+        {:error, reason} ->
+          assert {:error, reason} == false
+
+        {:ok, mat} ->
+          case OpenCV.imwrite(file, mat) do
+            {:error, reason} ->
+              assert {:error, reason} == false
+
+            :ok ->
+              assert File.exists?(file) == true
+
+              {file_str, file_exit} = System.cmd("file", [file])
+              assert file_exit == 0
+              assert String.match?(file_str, ~r/PC bitmap/) == true
+
+              {ok_error, binary} = File.read(file)
+
+              assert ok_error == :ok
+
+              assert binary ==
+                       <<66, 77, 58, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0, 40, 0, 0, 0, 1, 0, 0, 0, 1,
+                         0, 0, 0, 1, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 174, 90, 159, 0>>
+
+              case File.rm(file) do
+                :ok -> :ok
+                _ -> :ok
+              end
+          end
+      end
+    end
+
+    test "size = {3, 2}" do
+      img =
+        Nx.tensor(
+          [
+            [
+              [0x9F, 0x5A, 0xAE],
+              [0x00, 0x00, 0x00],
+              [0x00, 0x00, 0x00]
+            ],
+            [
+              [0xFF, 0xFF, 0xFF],
+              [0xFF, 0xFF, 0xFF],
+              [0x9F, 0x5A, 0xAE]
+            ]
+          ],
+          type: {:u, 8}
+        )
+
+      file = "/tmp/test_3_2.bmp"
+
+      case NxEvision.convert_nx_to_mat(img) do
+        {:error, reason} ->
+          assert {:error, reason} == false
+
+        {:ok, mat} ->
+          case OpenCV.imwrite(file, mat) do
+            {:error, reason} ->
+              assert {:error, reason} == false
+
+            :ok ->
+              assert File.exists?(file) == true
+
+              {file_str, file_exit} = System.cmd("file", [file])
+              assert file_exit == 0
+              assert String.match?(file_str, ~r/PC bitmap/) == true
+
+              {ok_error, binary} = File.read(file)
+
+              assert ok_error == :ok
+
+              assert binary ==
+                       <<66, 77, 78, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0, 40, 0, 0, 0, 3, 0, 0, 0, 2,
+                         0, 0, 0, 1, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 174, 90, 159, 0, 0, 0,
+                         174, 90, 159, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
+
+              case File.rm(file) do
+                :ok -> :ok
+                _ -> :ok
+              end
+          end
+      end
+    end
+  end
+
+  describe "imread and imwrite" do
+    test "png random image" do
+      img_expected = Nx.random_uniform({256, 256, 3}, 0, 256, type: {:u, 8})
+      file = "/tmp/test_random_256_256.png"
+
+      case NxEvision.convert_nx_to_mat(img_expected) do
+        {:error, reason} ->
+          assert {:error, reason} == false
+
+        {:ok, mat} ->
+          case OpenCV.imwrite(file, mat) do
+            {:error, reason} ->
+              assert {:error, reason} == false
+
+            :ok ->
+              assert File.exists?(file) == true
+
+              {file_str, file_exit} = System.cmd("file", [file])
+              assert file_exit == 0
+              assert String.match?(file_str, ~r/PNG image data/) == true
+
+              img_actual =
+                case OpenCV.imread(file) do
+                  {:ok, img} -> img
+                  _ -> assert false
+                end
+                |> NxEvision.convert_mat_to_nx()
+                |> case do
+                  {:ok, img} -> img
+                  _ -> assert false
+                end
+
+              assert img_expected == img_actual
+
+              case File.rm(file) do
+                :ok -> :ok
+                _ -> :ok
+              end
+          end
+      end
+    end
+  end
 end
